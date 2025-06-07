a73a475310529f91b0020b2ac9f2694dbeb33eb6
Kaal-09 <sakshamgautam369@gmail.com>
2025-06-07 16:16:57 +0530

    added line

--- src/components/CategoryBar.jsx
+++ src/components/CategoryBar.jsx
@@ -5,16 +5,19 @@ const categories = ['Economy', 'Sports', 'Tech', 'World', 'India'];
 
 const CategoryBar = () => {
   return (
-    <div className="flex flex-wrap gap-3 px-6 py-4 bg-white border-b">
-      {categories.map(category => (
-        <Button
-          key={category}
-          variant="outline"
-          className="rounded-full text-sm border-gray-300 text-gray-700 hover:bg-gray-100 cursor-pointer"
-        >
-          {category}
-        </Button>
-      ))}
+    <div>
+        <div className='w-[100%] h-0.25 bg-neutral-500 '></div>
+        <div className="flex flex-wrap gap-3 px-6 py-4 bg-white border-b">
+            {categories.map(category => (
+                <Button
+                key={category}
+                variant="outline"
+                className="rounded-full text-sm border-gray-300 text-gray-700 hover:bg-gray-100 cursor-pointer"
+                >
+                {category}
+                </Button>
+            ))}
+            </div>
     </div>
   );
 };
